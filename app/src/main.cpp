ef362019a418441f355f714c243df5082363c013
dejvino <dejvino@gmail.com>
2020-01-14 18:33:20 +0100

    Added backlight control driven by button interupts.
    Signed-off-by: dejvino <dejvino@gmail.com>

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,6 +5,8 @@ LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 #include <device.h>
 #include <display.h>
 #include <drivers/gpio.h>
+#include <sys/util.h>
+#include <inttypes.h>
 
 #include "GFX.h"
 GFX* tft;
@@ -12,27 +14,17 @@ GFX* tft;
 #define LED_PORT        DT_ALIAS_LED1_GPIOS_CONTROLLER
 #define LED             DT_ALIAS_LED1_GPIOS_PIN
 
-struct device *display_dev;
+struct device* display_dev;
 
-static void backlight_init(void)
-{
-	struct device *dev;
+void display_init() {
+#ifdef CONFIG_ST7789V_RGB565
+	//const size_t rgb_size = 2;
+#else
+	//const size_t rgb_size = 3;
+	LOG_ERR("RGB888 mode not supported. Aborting test.");
+	return;
+#endif
 
-	dev = device_get_binding(LED_PORT);
-    /* If you have a backlight, set it up and turn it on here */
-	gpio_pin_configure(dev, LED, GPIO_DIR_OUT);
-	gpio_pin_write(dev, LED, 0);
-}
-
-void delay(int ms) {
-	k_sleep(K_MSEC(ms));
-}
-
-uint16_t millis() { return 0; }
-
-void setup(void) {
-	backlight_init();
-	
 	display_dev = device_get_binding(DT_INST_0_SITRONIX_ST7789V_LABEL);
 
 	if (display_dev == NULL) {
@@ -45,6 +37,97 @@ void setup(void) {
 	tft = createGFX(display_dev);
 
 	tft->fillScreen(ST77XX_BLACK);
+}
+
+struct device* backlight_dev;
+
+static void backlight_enable(bool enable) {
+	gpio_pin_write(backlight_dev, LED, enable ? 0 : 1);
+}
+
+static void backlight_init(void)
+{
+	backlight_dev = device_get_binding(LED_PORT);
+    /* If you have a backlight, set it up and turn it on here */
+	gpio_pin_configure(backlight_dev, LED, GPIO_DIR_OUT);
+	backlight_enable(true);
+}
+
+void delay(int ms) {
+	k_sleep(K_MSEC(ms));
+}
+
+/* change this to use another GPIO port */
+#ifndef DT_ALIAS_SW0_GPIOS_CONTROLLER
+#ifdef SW0_GPIO_NAME
+#define DT_ALIAS_SW0_GPIOS_CONTROLLER SW0_GPIO_NAME
+#else
+#error SW0_GPIO_NAME or DT_ALIAS_SW0_GPIOS_CONTROLLER needs to be set in board.h
+#endif
+#endif
+#define PORT	DT_ALIAS_SW0_GPIOS_CONTROLLER
+
+/* change this to use another GPIO pin */
+#ifdef DT_ALIAS_SW0_GPIOS_PIN
+#define PIN     DT_ALIAS_SW0_GPIOS_PIN
+#else
+#error DT_ALIAS_SW0_GPIOS_PIN needs to be set in board.h
+#endif
+
+#define EDGE    (GPIO_INT_EDGE | GPIO_INT_DOUBLE_EDGE)
+
+/* change this to enable pull-up/pull-down */
+#ifndef DT_ALIAS_SW0_GPIOS_FLAGS
+#ifdef DT_ALIAS_SW0_GPIOS_PIN_PUD
+#define DT_ALIAS_SW0_GPIOS_FLAGS DT_ALIAS_SW0_GPIOS_PIN_PUD
+#else
+#define DT_ALIAS_SW0_GPIOS_FLAGS 0
+#endif
+#endif
+#define PULL_UP DT_ALIAS_SW0_GPIOS_FLAGS
+
+struct device* button_dev;
+
+bool isButtonPressed() {
+	u32_t val = 0U;
+	gpio_pin_read(button_dev, PIN, &val);
+	return val == 1U;
+}
+
+void button_pressed(struct device *gpiob, struct gpio_callback *cb,
+		u32_t pins)
+{
+	backlight_enable(isButtonPressed());
+}
+
+static struct gpio_callback gpio_cb;
+
+void button_init()
+{
+	u32_t button_out = 1U;
+
+	button_dev = device_get_binding(PORT);
+	if (!button_dev) {
+		printk("error\n");
+		return;
+	}
+
+	gpio_pin_configure(button_dev, PIN,
+			GPIO_DIR_IN | GPIO_INT |  PULL_UP | EDGE);
+
+	gpio_init_callback(&gpio_cb, button_pressed, BIT(PIN));
+	gpio_add_callback(button_dev, &gpio_cb);
+	gpio_pin_enable_callback(button_dev, PIN);
+
+//port 15 has to be high in order for the button to work
+	gpio_pin_configure(button_dev, 15,GPIO_DIR_OUT); //push button out
+	gpio_pin_write(button_dev, 15, button_out); //set port high
+}
+
+void setup(void) {
+	backlight_init();
+	button_init();
+	display_init();	
 }
 
 u16_t clock = 0;
@@ -67,7 +150,8 @@ inline void loop() {
 	tft->print(clockText);
 	tft->flushBuffer();
 
-	delay(100);
+	backlight_enable(isButtonPressed());
+	delay(999);
 
 	clock++;
 }
@@ -75,14 +159,6 @@ inline void loop() {
 void main(void)
 {
 	LOG_INF("PineTime Hermes starting.");
-
-#ifdef CONFIG_ST7789V_RGB565
-	//const size_t rgb_size = 2;
-#else
-	//const size_t rgb_size = 3;
-	LOG_ERR("RGB888 mode not supported. Aborting test.");
-	return;
-#endif
 
 	setup();
 
